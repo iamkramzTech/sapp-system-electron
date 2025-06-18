cba5a509bab6f85a5ea323c97b587665af7cda29
iamKramzTech <bernaldezm88@yahoo.com>
2025-06-18 14:12:20 +0800

    moved to src folder

--- config/db.config.js
+++
@@ -1,8 +0,0 @@
-// DB configuration can be added here
-require('dotenv').config();
-module.exports = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-};
